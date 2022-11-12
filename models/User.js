517ab6635168396c100094099009988932c02171
Duder <thedudeabides1981@hotmail.com>
2022-11-12 01:28:41 -0800

    created user and thought schemas

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    thoughts: {
+        type: Array
+    }, 
+    friends: {
+        type: Array
+    }
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    }
+});
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
